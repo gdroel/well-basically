9ea091185db0167bff7755f16d4db55c0f41d187
Gabe Roeloffs <gdroel@gmail.com>
2014-09-22 20:43:21 -0700

    rudimentary search working

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -15,7 +15,7 @@
 <script type="text/javascript"
   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBoaSu9IZTRrCkY1tTnMibgHg-uwB8aduk">
 </script>
-
+ <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
 
 <script type="text/javascript">
 var map = null;
@@ -24,6 +24,7 @@ var gmarkers = [];
 
 var wells = <?php echo $addresses ?>;
 function initialize() {
+var placesArray = [];
   var mapOptions = {
     zoom: 10,
     center: new google.maps.LatLng(35, -121)
@@ -32,7 +33,34 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map-canvas'),
                               mapOptions);
 
+    // Create the search box and link it to the UI element.
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+      // For each place, get the icon, place name, and location.
+      var bounds = new google.maps.LatLngBounds();
+      for (var i = 0, place; place = places[i]; i++) {
+
+        placesArray.push(marker);
+
+        bounds.extend(place.geometry.location);
+      }
+
+      map.fitBounds(bounds);
+    });
+
   google.maps.event.addListener(map, 'bounds_changed', function(){setMarkers(map, wells)});
+
 }
 
 
@@ -112,6 +140,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
+  <input id="pac-input" class="controls" type="text" placeholder="Search Box">
       </ul>
       <ul class="nav navbar-nav navbar-right">
         @if(Auth::check())
